14b8fe6d9bcc035ea31548a4675778e19ff145e8
minaje0917 <k050918@naver.com>
2023-08-25 10:11:07 +0900

    :sparkles: Add :: tuist

---
+++ Project.swift
@@ -0,0 +1,97 @@
+import ProjectDescription
+
+let projectName = "Daily"
+let organizationName = "Daily"
+let bundleID = "Minjae.daily"
+let targetVersion = "15.0"
+
+// Creates our project using a helper function defined in ProjectDescriptionHelpers
+let project = Project(
+    name: projectName,
+    organizationName: organizationName,
+    packages: [
+        .remote(
+            url: "https://github.com/RxSwiftCommunity/RxFlow.git",
+            requirement: .upToNextMajor(from: "2.10.0")
+        ),
+        .remote(
+            url: "https://github.com/ReactiveX/RxSwift.git",
+            requirement: .upToNextMajor(from: "6.6.0")
+        ),
+        .remote(
+            url: "https://github.com/SnapKit/SnapKit.git",
+            requirement: .upToNextMajor(from: "5.0.1")
+        ),
+        .remote(
+            url: "https://github.com/devxoul/Then",
+            requirement: .upToNextMajor(from: "2")
+        ),
+        .remote(
+            url: "https://github.com/Moya/Moya.git",
+            requirement: .upToNextMajor(from: "15.0.0")
+        ),
+        .remote(
+            url: "https://github.com/onevcat/Kingfisher.git",
+            requirement: .upToNextMajor(from: "7.0.0")
+        ),
+        .remote(
+            url: "https://github.com/ReactorKit/ReactorKit.git",
+            requirement: .upToNextMajor(from: "3.2.0")
+        ),
+        .remote(
+            url: "https://github.com/marcosgriselli/ViewAnimator.git",
+            requirement: .upToNextMajor(from: "3.1.0")
+        ),
+        .remote(
+            url: "https://github.com/WenchaoD/FSCalendar.git",
+            requirement: .upToNextMajor(from: "2.8.3")
+        ),
+        .remote(
+            url: "https://github.com/kaishin/Gifu.git",
+            requirement: .upToNextMajor(from: "3.2.2")
+        ),
+        .remote(
+            url: "https://github.com/Datt1994/DPOTPView.git",
+            requirement: .exact("1.5.12")
+        )
+        
+    ],
+    settings: nil,
+    targets: [
+        Target(name: projectName,
+               platform: .iOS,
+               product: .app, // unitTests, .appExtension, .framework, dynamicLibrary, staticFramework
+               productName: projectName,
+               bundleId: bundleID,
+               deploymentTarget: .iOS(
+                targetVersion: targetVersion,
+                devices: [.iphone, .ipad]
+               ),
+               infoPlist: .file(path: "\(projectName)/Info.plist"),
+               sources: ["\(projectName)/**"],
+               resources: ["\(projectName)/Resources/**"],
+               dependencies: [
+                .package(product: "RxFlow"),
+                .package(product: "RxSwift"),
+                .package(product: "RxCocoa"),
+                .package(product: "SnapKit"),
+                .package(product: "Then"),
+                .package(product: "Moya"),
+                .package(product: "Kingfisher"),
+                .package(product: "ReactorKit"),
+                .package(product: "ViewAnimator"),
+                .package(product: "FSCalendar"),
+                .package(product: "Gifu"),
+                .package(product: "DPOTPView")
+               ] // tuist generate할 경우 pod install이 자동으로 실행
+              )
+    ],
+    schemes: [
+        Scheme(name: "\(projectName)-Debug"),
+        Scheme(name: "\(projectName)-Release")
+    ],
+    fileHeaderTemplate: nil,
+    additionalFiles: [],
+    resourceSynthesizers: []
+)
+
--- daily/Sources/Presentation/Main/TabBar/VM/TabBarViewModel.swift
+++ daily/Sources/Presentation/Main/TabBar/VM/TabBarViewModel.swift
@@ -1,16 +1,1 @@
-//
-//  TabBarViewModel.swift
-//  daily
-//
-//  Created by 선민재 on 2023/03/14.
-//
 
-import UIKit
-import RxCocoa
-import RxSwift
-import RxFlow
-
-class TabBarViewModel: BaseViewModel, Stepper{
-    var steps = PublishRelay<Step>()
-
-}
