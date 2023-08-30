c35a3143d6caa22603f3c16eed78a542f34b53ba
minaje0917 <k050918@naver.com>
2023-08-30 17:13:00 +0900

    :sparkles: Add :: AppleOauth

--- daily/Sources/Presentation/Auth/OnBoarding/VC/OnBoardingViewController.swift
+++ daily/Sources/Presentation/Auth/OnBoarding/VC/OnBoardingViewController.swift
@@ -1,15 +1,9 @@
-//
-//  ViewController.swift
-//  daily
-//
-//  Created by 선민재 on 2022/10/27.
-//
-
 import UIKit
 import Then
 import SnapKit
 import RxCocoa
 import ViewAnimator
+import AuthenticationServices
 
 class OnBoardingViewController: BaseViewController<OnBoardingReactor>{
     
@@ -59,75 +53,19 @@ class OnBoardingViewController: BaseViewController<OnBoardingReactor>{
         $0.alignment = .center
     }
     
-    private let bottomStackView = UIStackView().then {
-        $0.spacing = 8
-        $0.axis = .horizontal
-        $0.alignment = .center
-    }
-    
-    private var signUpButton = UIButton().then {
-        let text = NSAttributedString(string: "회원가입")
-        $0.setAttributedTitle(
-            text,
-            for: .normal
-        )
-        $0.titleLabel?.font = UIFont.systemFont(
-            ofSize: 16,
-            weight: .semibold
-        )
-        $0.setTitleColor(
-            UIColor.white,
-            for: .normal
-        )
-        $0.backgroundColor = UIColor.mainColor
-        $0.layer.cornerRadius = 20
-    }
-    
-    private let haveAccountText = UILabel().then {
-        $0.text = "계정이 있으신가요?"
-        $0.font = UIFont.systemFont(
-            ofSize: 16,
-            weight: .regular
-        )
-        $0.textColor = UIColor(
-            red: 238/255,
-            green: 232/255,
-            blue: 232/255,
-            alpha: 1.00
-        )
-    }
-    
-    private var signInButton = UIButton().then {
-        let text = NSAttributedString(string: "로그인")
-        $0.setAttributedTitle(
-            text,
-            for: .normal
-        )
-        $0.titleLabel?.font = UIFont.systemFont(
-            ofSize: 16,
-            weight: .bold
-        )
-        $0.setTitleColor(
-            UIColor.mainColor,
-            for: .normal
-        )
-        $0.backgroundColor = UIColor(
-            red: 1,
-            green: 1,
-            blue: 1,
-            alpha: 0
-        )
+    private let signInWithAppleButton = ASAuthorizationAppleIDButton(
+        type: .signIn,
+        style: .black
+    ).then {
+        $0.cornerRadius = 10
     }
     
     override func addView() {
-        [backgroundImage,dailyLogo,topStackView,signUpButton,bottomStackView].forEach {
+        [backgroundImage,dailyLogo,topStackView,signInWithAppleButton].forEach {
             view.addSubview($0)
         }
         [mainExplainText,subExplainText].forEach {
             topStackView.addArrangedSubview($0)
-        }
-        [haveAccountText,signInButton].forEach {
-            bottomStackView.addArrangedSubview($0)
         }
     }
     
@@ -138,7 +76,7 @@ class OnBoardingViewController: BaseViewController<OnBoardingReactor>{
             AnimationType.from(direction: .bottom, offset: 211)
         ], initialAlpha: 0, finalAlpha: 1, delay: 0, duration: 1.25, options: .curveEaseInOut)
         UIView.animate(views: [
-            mainExplainText, subExplainText, signUpButton,haveAccountText, signInButton
+            mainExplainText, subExplainText, signInWithAppleButton
         ], animations: [
             AnimationType.from(direction: .left, offset: 100)
         ], initialAlpha: 0, finalAlpha: 1, delay: 0, duration: 1, options: .curveEaseInOut)
@@ -157,25 +95,17 @@ class OnBoardingViewController: BaseViewController<OnBoardingReactor>{
             $0.centerX.equalToSuperview()
             $0.top.equalTo(dailyLogo.snp.bottom).offset(24)
         }
-        signUpButton.snp.makeConstraints {
+        signInWithAppleButton.snp.makeConstraints {
             $0.centerX.equalToSuperview()
             $0.leading.trailing.equalToSuperview().inset(20)
-            $0.top.equalTo(topStackView.snp.bottom).offset((bounds.height) / 2.8)
-            $0.height.equalTo(60)
-        }
-        bottomStackView.snp.makeConstraints {
-            $0.top.equalTo(signUpButton.snp.bottom).offset(16)
-            $0.trailing.equalTo(view.snp.trailing).inset((bounds.width) / 3.86)
+            $0.bottom.equalToSuperview().inset(100)
+            $0.height.equalTo(56)
         }
     }
     
     override func bindView(reactor: OnBoardingReactor) {
-        signInButton.rx.tap
-            .map { OnBoardingReactor.Action.signInButtonTap }
-            .bind(to: reactor.action)
-            .disposed(by: disposeBag)
-        signUpButton.rx.tap
-            .map { OnBoardingReactor.Action.signUpButtonTap }
+        signInWithAppleButton.rx.controlEvent(.touchUpInside)
+            .map { OnBoardingReactor.Action.signInWithAppleButtonDidTap }
             .bind(to: reactor.action)
             .disposed(by: disposeBag)
     }
