240531ddc5c9351dd2272b708ee02ab077745d78
minaje0917 <k050918@naver.com>
2023-09-05 14:00:25 +0900

    :zap: Change :: BaseURL

--- daily/Sources/Data/General/BaseURL.swift
+++ daily/Sources/Data/General/BaseURL.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 struct BaseURL {
-    static let baseURL = "https://port-0-daily-server-v4-r8xoo2mlef98yij.sel3.cloudtype.app/api/v1"
+    static let baseURL = "port-0-daily-server-1xxfe2blm5msla7.sel5.cloudtype.app/api/v1"
 }
