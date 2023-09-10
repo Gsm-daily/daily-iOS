24943f31ae99319aa08df0c514a42c75f3989b7c
minaje0917 <k050918@naver.com>
2023-09-10 14:31:32 +0900

    :sparkles: Add :: Diary Services

--- daily/Sources/Data/Request/Diary/WriteDiaryRequest.swift
+++ daily/Sources/Data/Request/Diary/WriteDiaryRequest.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-struct DiaryRequest: Codable {
+struct WriteDiaryRequest: Codable {
     let content: String
     let date: String
     
---
+++ daily/Sources/Data/Services/Diary/DiaryServices.swift
@@ -0,0 +1,45 @@
+import Foundation
+import Moya
+
+enum DiaryServices {
+    case writeDiary(param: WriteDiaryRequest, authorization: String)
+}
+
+
+extension DiaryServices: TargetType {
+    public var baseURL: URL {
+        return URL(string: BaseURL.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .writeDiary:
+            return "/diary"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .writeDiary:
+            return .post
+        }
+    }
+    
+    var sampleData: Data {
+        return "@@".data(using: .utf8)!
+    }
+    
+    var task: Task {
+        switch self {
+        case let .writeDiary(param,_):
+            return .requestJSONEncodable(param)
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        case let .writeDiary(_, authorization):
+            return ["Content-Type" :"application/json", "Authorization": authorization]
+        }
+    }
+}
