bdf3aa750ca21358e0a24bbe98c557650c3f85d2
minaje0917 <k050918@naver.com>
2023-10-05 13:59:05 +0900

    :sparkles: Add :: KakaoAuth handleOpenUrl()

--- daily/Sources/Application/Flow/SceneDelegate.swift
+++ daily/Sources/Application/Flow/SceneDelegate.swift
@@ -1,25 +1,41 @@
 import UIKit
 import RxCocoa
 import RxFlow
+import KakaoSDKAuth
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-        var coordinator = FlowCoordinator()
-
-        func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-            guard let windowScene = (scene as? UIWindowScene) else { return }
-            let window = UIWindow(windowScene: windowScene)
-            self.window = window
-            let appFlow = AppFlow(window: window)
-            self.coordinator.coordinate(flow: appFlow, with: AppStepper())
-            window.makeKeyAndVisible()
-            
-    //        Flows.use(appFlow, when: .created) { root in
-    //            window.rootViewController = root
-    //            window.makeKeyAndVisible()
-    //        }
+    var coordinator = FlowCoordinator()
+    
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
+        let appFlow = AppFlow(window: window)
+        self.coordinator.coordinate(flow: appFlow, with: AppStepper())
+        window.makeKeyAndVisible()
+        
+        //        Flows.use(appFlow, when: .created) { root in
+        //            window.rootViewController = root
+        //            window.makeKeyAndVisible()
+        //        }
+    }
+    
+    func scene(
+        _ scene: UIScene,
+        openURLContexts URLContexts: Set<UIOpenURLContext>
+    ) {
+        if let url = URLContexts.first?.url {
+            if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                _ = AuthController.handleOpenUrl(url: url)
+            }
         }
+    }
     
     func sceneDidDisconnect(_ scene: UIScene) {}
 
